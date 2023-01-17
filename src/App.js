b8722348bc666753ea27fff72863f683911fe2eb
halisa <halisasaipulaa@gmail.com>
2023-01-17 15:32:26 -0800

    Implemented React Context API

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
 import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
 import './App.css';
+import { useStateContext } from "./contexts/ContextProvider";
 
 const App = () => {
-    const activeMenu = true;
+    const {activeMenu} = useStateContext();
     
     return (
         <div>
