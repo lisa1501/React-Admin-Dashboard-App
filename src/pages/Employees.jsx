ba20afd0238e2e454272a960125fa57c820852e8
Lisa S <halisasaipulla@gmail.com>
2023-01-21 10:45:59 -0800

    implemented Data Tables--Employees Page Search feature

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { GridComponent, ColumnsDirective, ColumnDirective,  Page, ExcelExport, Search, Inject } from '@syncfusion/ej2-react-grids';
+import { GridComponent, ColumnsDirective, ColumnDirective, Page, Search, Inject, Toolbar } from '@syncfusion/ej2-react-grids';
 
 import { employeesData, employeesGrid } from '../data/dummy';
 import { Header } from '../components';
@@ -10,16 +10,17 @@ const Employees = () => {
         <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
             <Header category="Page" title="Employees" />
             <GridComponent
-                id="gridcomp"
                 dataSource={employeesData}
                 allowPaging
-                allowSorting>
+                allowSorting
+                toolbar={['Search']}
+                width="auto">
                 <ColumnsDirective>
                     {employeesGrid.map((item, index) => (
                         <ColumnDirective key={index} {...item} />
                     ))}
                 </ColumnsDirective>
-                <Inject services={[Page, Search ]} />
+                <Inject services={[Page, Search, Toolbar]} />
             </GridComponent>
 
         </div>
