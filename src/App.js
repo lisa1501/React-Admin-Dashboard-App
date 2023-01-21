639d25471617698cfa002fbe363cf7034f44d6eb
halisa <halisasaipulaa@gmail.com>
2023-01-21 12:58:32 -0800

    Implemented the Settings Theme Options and Theme Colors

--- src/App.js
+++ src/App.js
@@ -46,6 +46,7 @@ const App = () => {
                         
                         
                         <div>
+                            <ThemeSettings />
                             <Routes>
                                 {/* Dashboard */}
                                 <Route path="/" element={<Ecommerce />}/>
