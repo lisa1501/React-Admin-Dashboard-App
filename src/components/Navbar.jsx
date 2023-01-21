57d2d514706c5715100fe1f2246c039e8d291aa6
halisa <halisasaipulaa@gmail.com>
2023-01-21 14:20:39 -0800

    Implemented Theme Settings Section functionality work

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,7 +29,7 @@ const NavButton = ({ title, customFunc, icon, color, dotColor}) => (
     </TooltipComponent>
 )
 const Navbar = () => {
-    const {activeMenu, setActiveMenu, handleClick, isClicked, setIsClicked, screenSize, setScreenSize} = useStateContext()
+    const {activeMenu, setActiveMenu, handleClick, isClicked, setIsClicked, screenSize, setScreenSize, currentColor} = useStateContext()
     
     useEffect(() => {
         const handleResize = () => setScreenSize(window.innerWidth);
@@ -56,21 +56,21 @@ const Navbar = () => {
                 <NavButton 
                     title="Cart" 
                     customFunc={() => handleClick('cart')}
-                    color="blue" 
+                    color={currentColor}
                     icon={< FiShoppingCart />}
                 />
                 <NavButton 
                     title="Chat" 
                     dotColor="#03C9D7"
                     customFunc={() => handleClick('chat')}
-                    color="blue" 
+                    color={currentColor}
                     icon={< BsChatLeft />}
                 />
                 <NavButton 
                     title="Notifications" 
                     dotColor="#03C9D7"
                     customFunc={() => handleClick('notification')}
-                    color="blue" 
+                    color={currentColor}
                     icon={< RiNotification3Line />}
                 />
                 <TooltipComponent
