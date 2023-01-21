c6dcd01e109c960935efd23e0d03e53f46ed7005
Lisa S <halisasaipulla@gmail.com>
2023-01-21 10:54:47 -0800

    implemented Data Tables--Customer Page

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -6,7 +6,25 @@ import { Header } from '../components';
 
 const Customers = () => {
     return (
-        <div>Customers</div>
+        <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+            <Header category="Page" title="customers" />
+            <GridComponent
+                dataSource={customersData}
+                allowPaging
+                allowSorting
+                toolbar={['Delete']}
+                editSettings={{ allowDeleting: true, 
+                allowEditing: true}}
+                width="auto">
+                <ColumnsDirective>
+                    {customersGrid.map((item, index) => (
+                        <ColumnDirective key={index} {...item} />
+                    ))}
+                </ColumnsDirective>
+                <Inject services={[Page, Toolbar, Selection, Edit, Sort, Filter]} />
+            </GridComponent>
+
+        </div>
     )
 }
 
