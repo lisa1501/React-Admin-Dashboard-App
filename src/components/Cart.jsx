0ee47523a1231070d6ba477f4964bed07e725ca3
Lisa S <halisasaipulla@gmail.com>
2023-02-06 11:45:01 -0800

    implemented Chat Component

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -21,7 +21,7 @@ const Cart = () => {
                             style={{ color:'rgb(153, 171, 180)', 
                             borderRadius: '50%' }}
                             className="text-2xl p-3
-                            hover:drop-shadow-xl hover:bg-light-gray">
+                            hover:drop-shadow-xl hover:bg-light-gray ">
                             <MdOutlineCancel />
                     </button>
                 </div>
